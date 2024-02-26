95eb2aaf6b636a1aa7f0cf534e3a9c3359c5a02b
Zeus Elderfield <elderfieldzeus24@gmail.com>
2024-02-26 22:36:29 +0800

    added exam 1 of monopoly

--- 4_File Handling/testingBinaryFileHandling.c
+++ 4_File Handling/testingBinaryFileHandling.c
@@ -1,18 +1,20 @@
+//TESTING OUT FILE HANDLING FUNCTIONS
+
 #include <stdio.h>
 #include <string.h>
 
 int main() {
     FILE *fptr;
-    if((fptr = fopen("./binary.txt", "r+")) == NULL) {
+    if((fptr = fopen("./binary.dat", "rb+")) == NULL) {
         printf("FILE NOT PRESENT\n");
-        if((fptr = fopen("./binary.txt", "w+")) == NULL) {
+        if((fptr = fopen("./binary.dat", "wb+")) == NULL) {
             printf("FILE NOT WRITTEN\n");
             return 1;
         }
     }
     else {
         printf("FILE PRESENT\n");
-        if((fptr = fopen("./binary.txt", "a+")) == NULL) {
+        if((fptr = fopen("./binary.dat", "ab+")) == NULL) {
             printf("FILE CANNOT BE READ");
             return 1;
         }
@@ -22,11 +24,16 @@ int main() {
     char printed[100];
     printf("Enter word to be placed in file: ");
     scanf(" %s", string);
-    strcat(string, "\n");
-    fwrite(string, sizeof(char), strlen(string), fptr);
+    strcat(string, "\0");
+    fwrite(string, sizeof(char), strlen(string) + 1, fptr);
     rewind(fptr);
-    while(fread(printed, sizeof(char), 100, fptr) != 0) {
-        
+    int i = 0;
+    while(fread(printed + i, sizeof(char), 1, fptr) != 0) {
+        if(printed[i] == '\0') {
+            printf("%s\n", printed);
+            i = -1;
+        }
+        i++;
     }
     fclose(fptr);
     return 0;
